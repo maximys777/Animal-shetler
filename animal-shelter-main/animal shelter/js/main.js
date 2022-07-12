11b0a7fda82372ac914d489bf948b28bb4dbb088
Maksym <48604958+maximys777@users.noreply.github.com>
2022-07-12 23:13:42 +0300

    Add files via upload

---
+++ animal-shelter-main/animal shelter/js/main.js
@@ -0,0 +1,153 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*========== REMOVE MENU MOBILE ==========*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*========== CHANGE BACKGROUND HEADER ==========*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    //When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >=50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*========== SCROLL SECTIONS ACTIVE LINK ==========*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scollY = window.pageYOffset
+
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id')
+
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+        document.querySelector('nav.menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*========== SHOW SCROLL UP ==========*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*========== SCROLL SECTIONS ACTIVE LINK ==========*/
+const section = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== SHOW FORM ====================*/
+const formCard = document.getElementById('form-drop'),
+      formToggle = document.getElementById('form-toggle'),
+      formClose = document.getElementById('close-form')
+
+/*===== FORM SHOW =====*/
+/* Validate if constant exists */
+if(formToggle){
+    formToggle.addEventListener('click', () =>{
+        formCard.classList.add('show-menu-form')
+    })
+}
+
+/*===== FORM HIDDEN =====*/
+/* Validate if constant exists */
+if(formClose){
+    formClose.addEventListener('click', () =>{
+        formCard.classList.remove('show-menu-form')
+    })
+}
+
+/*========== SHELTER SWIPER ==========*/
+let swiperShelter = new Swiper(".shelter__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+      }, 
+});
+
+
+/*========== FOOD SWIPER ==========*/
+let swiperFood = new Swiper(".food__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+      }, 
+});
